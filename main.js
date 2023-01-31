b6937d9877d59b33c77769b895bdf5f98bb7cba2
Diyor2405 <nurullayevdiyor2005@gmail.com>
2023-01-31 16:16:40 +0500

    main.js

---
+++ main.js
@@ -0,0 +1,179 @@
+var a = 'If else ga oid masalalar'
+
+console.log(a);
+
+alert('If else va For mazvulari')
+
+// for ( let i = 1 ; i <= 100 ; i++ ){
+//    if(i % 2 === 0){
+//     console.log(i + '-------juft son')
+//    }else{
+//     console.log(i + '-----toq son');
+//    }
+// }
+
+
+// for ( let i = 0 ; i <= 99 ; i++ ){
+//   if(i % 3 === 0){
+//     console.log(i + "---3ga bolinuvchi sonlar");
+//   }else{
+//      console.log(i + '---juft son');
+//   }
+// }
+
+// let count = 0;
+
+// count = 1;
+
+// for ( let i = 0 ; i <= 100 ; i++){
+//  if (i % 2 === 0){
+//   count += 1
+// }
+// }
+
+//   console.log(count)
+
+
+// let a = 2;
+// let b = -1;
+// let c = 5;
+// let d = 9;
+
+// if(a > 0 , b < 0 , c > 0 , d > 0){
+//   console.log('hammasi musbat');
+// }else {
+//   console.log('manfiy');
+// }
+
+                   //songa qarab hafta kunlari chiqarsin
+
+// var s1 = 'd';
+// var s2 = 's';
+// var s3 = 'cho';
+// var s4 = 'p';
+// var s5 = 'juma';
+// var s6 = 'sh';
+
+// console.log(s4);
+
+
+                // musbat bolsa bita qoshsin manfiy bolsa ayir  sin           //////////
+// let i = 4;
+
+// if(i > 0 ){
+//   console.log( i++);
+// }else(i < 0); {
+//   console.log(i--);
+// }
+
+                  // katasini aniqlovchi dastur
+
+// let num = Math.max(2,3,5,6);
+// console.log(num);
+
+
+                // //kichik sonni aniqlovchi dastur
+
+// let num = Math.min(-1 , -7 , 3)
+// console.log(num);
+
+
+                    ////// jinsiga qarab aniqlash                       /  /////
+
+// let a = "erkak"
+// let b = "ayol"
+
+// if (a) {
+//   console.log(a);
+// }
+// else (b); {
+//   console.log(b);
+// }
+
+
+//                ////bahoga qarab stipendiyani aniqlash
+
+// let baho = 5 ;
+
+// if (baho === 2) {
+//  console.log("stipendiya 100ming");
+// }else if ( baho < 2 ){
+//   console.log('stipendiya olmaysiz');
+// }else if (baho === 3){
+//   console.log("stipendiya 250ming");
+// }else if (baho === 4){
+//   console.log('stipendiya 400ming');
+// }else if (baho === 5){
+//   console.log('stipendiya 555 ming');
+//  }
+
+                       //// /yiliga qarab yoshini aniqlash dasturi
+
+// let yosh = 2005 ;
+
+// console.log(2023 - yosh);
+
+
+                         ////a b c sonlar   shartu - a <= b <= c
+
+// let A = 2 ;
+// let B = 4 ;
+// let C = 9 ;
+
+// if (A <= B , B <= C ) {
+//   console.log(true);
+// }else{
+//   console.log(false);
+// }
+
+           ///////ikta son bor bittasi musbat
+
+// let a = 34;
+// let b = 99;
+
+// if (a % 2 === 0 , b % 2 === 0) {
+//   console.log('juft son');
+// }else{
+//   console.log('toq son');
+// }
+
+
+
+               ///////// a b c son berilgan bittasi musbat
+
+// let a = 3;
+// let b = -6;
+// let c = -5;
+
+// if (a > 0 , b > 0 , c > 0) {
+//   console.log('juft sonlar ');
+// }else{
+//   console.log('toq son mavjud');
+// }
+
+                   /////////// kompyuter nomiga qarab narxi chiquvchi dastur          //////////////////////
+
+// let hp = '500$';
+// let mac = '1000$';
+// let asus = '450$';
+// let lenovo = '350$';
+
+// if (hp) {
+//   console.log(hp);
+// }else if(mac){
+//   console.log(mac);
+// }
+
+
+
+          /////////sonlar berilgan orsidan murakkablarini aniqlang
+
+// let i = 1 ;
+
+// for (let i = 0; i < 999; i++) {
+//   if ( i % 2 === 0) {
+//    console.log(i+ '----murakkab son');
+//   }else{
+//     console.log( i+ '-------tup son');
+//   }
+// }
